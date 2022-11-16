6f63e28ed7d313c9bfdf697e41507878d79bc3b4
JorgePlazaC <chocheplaza@hotmail.com>
2022-11-16 17:28:38 -0300

    Antes de instalar dropdown

--- screens/AdmProductos.jsx
+++ screens/AdmProductos.jsx
@@ -1,15 +1,197 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Button, Dimensions, FlatList, Image, TouchableOpacity } from 'react-native'
+import React, {useState,useEffect,useContext} from 'react'
 import { useNavigation } from '@react-navigation/native'
+import axios from 'axios'
 
+import RestaurantContext from '../src/components/RestaurantContext'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Modal } from 'react-native'
+import { TextInput } from 'react-native'
+
+const width = Dimensions.get('window')
 
 export default function AdmProductos() {
 
   const navigation = useNavigation()
+
+  const baseUrl = 'http://10.0.2.2:8000'
+  const url = `${baseUrl}/api/productos`
+  const urlCategorias = `${baseUrl}/api/productos`
+  const {mesa, setMesa, carro, setCarro,carroAgregado,setCarroAgregado} = useContext(RestaurantContext)
+
+  const [arrayProductos , setArrayProductos] = useState([])
+  const [arrayCategorias , setArrayCategorias] = useState([])
+  const [cargando,setCargando] = useState(true)
+  const [modalVisible,setModalVisible] = useState(false)
+  const [modalEdicionVisible,setEdicionModalVisible] = useState(false)
+  const [modalBorrarVisible,setBorrarModalVisible] = useState(false)
+  const [productosEdit,setProductosEdit] = useState()
+
+  let inputProducto = ""
+
+  useEffect(() => {
+    (async () =>{
+      await fetchProductos()
+      await fetchCategorias()
+    })()
+}, [])
+
+
+// Invoking get method to perform a GET request
+const fetchProductos = async () => {
+try{
+  const response = await axios.get(url)
+  setArrayProductos(response.data)
+  setCargando(false)
+  console.log(arrayProductos)
+}catch(error){
+  
+  console.log(error)
+  console.log({url})
+}
+}
+
+const fetchCategorias = async () => {
+  try{
+    const response = await axios.get(urlCategorias)
+    setArrayCategorias(response.data)
+  }catch(error){
+    
+    console.log(error)
+    console.log({url})
+  }
+  }
+
+const Confirmar = async () =>{
+  try {
+    const response = await axios.post(url, {nombre:inputProducto})
+    console.log(response.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const ModalEdicion = (producto) =>{
+  setProductosEdit(producto)
+  setEdicionModalVisible(true)
+}
+
+const ModalBorrar = (producto) =>{
+  setProductosEdit(producto)
+  setBorrarModalVisible(true)
+}
+
+const EditarCategoria = async () => {
+  let urlEdicion = `${url}/${productosEdit.id}`
+  console.log(urlEdicion)
+  try {
+    const response = await axios.put(urlEdicion, {nombre:inputProducto})
+    console.log(response.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const BorrarCategoria = async () => {
+  let urlBorrar = `${url}/${productosEdit.id}`
+  console.log(urlBorrar)
+  try {
+    const response = await axios.delete(urlBorrar)
+    console.log(response.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+      <Text style = {styles.text}>{title.nombre}</Text>
+      <TouchableOpacity style={styles.button} onPress={()=>{ModalEdicion(title)}}>
+          <Image style = {styles.image} source={require("../src/images/editar.png")}/>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={()=>{ModalBorrar(title)}}>
+          <Image style = {styles.image} source={require("../src/images/borrar.png")}/>
+        </TouchableOpacity>
+  </View>
+)
+
+const renderItem = ({ item }) => (
+  <Item title={item} />
+)
+
   return (
     <View style = {styles.viewBody}>
-      <Text>Administrador de productos</Text>
-       
+      <FlatList
+        style = {styles.flatList}
+        data={arrayProductos
+  }
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      /> 
+      <Button
+      style={styles.button}
+      title="Agregar producto"
+      onPress={() => {console.log(setModalVisible(true))}}
+      />
+      <Modal visible = {modalVisible} animationType = {'slide'}>
+        <View style = {styles.modalBackGround}>
+          <View style = {styles.modalContainer}>
+            <Text>Ingrese el nombre</Text>
+            <TextInput placeholder='Nombre' onChangeText={(text) => inputProducto = text}/>
+            <Text>Ingrese la descripción</Text>
+            <TextInput placeholder='Descripción' onChangeText={(text) => inputProducto = text}/>
+            <Text>Ingrese el precio</Text>
+            <TextInput placeholder='Precio' onChangeText={(text) => inputProducto = text}/>
+            <Text>Ingrese el stock</Text>
+            <TextInput placeholder='Stock' onChangeText={(text) => inputProducto = text}/>
+            <Button
+            style={styles.button}
+            title="Confirmar"
+            onPress={() => {Confirmar().then(fetchProductos).finally(setModalVisible(false))}}
+            />
+            <Button
+            style={styles.button}
+            title="Cancelar"
+            onPress={() => {setModalVisible(false)}}
+            />
+          </View>
+        </View>
+      </Modal>
+      <Modal visible = {modalEdicionVisible} animationType = {'slide'}>
+        <View style = {styles.modalBackGround}>
+          <View style = {styles.modalContainer}>
+            <Text>Cambiar nombre</Text>
+            <TextInput placeholder='Nuevo nombre' onChangeText={(text) => inputProducto = text}/>
+            <Button
+            style={styles.button}
+            title="Cambiar"
+            onPress={() => {EditarCategoria().then(fetchProductos).finally(setEdicionModalVisible(false))}}
+            />
+            <Button
+            style={styles.button}
+            title="Cancelar"
+            onPress={() => {setEdicionModalVisible(false)}}
+            />
+          </View>
+        </View>
+      </Modal>
+      <Modal visible = {modalBorrarVisible} animationType = {'slide'}>
+        <View style = {styles.modalBackGround}>
+          <View style = {styles.modalContainer}>
+            <Text>¿Está seguro que desea eliminar la categoria?</Text>
+            <Button
+            style={styles.button}
+            title="Sí"
+            onPress={() => {BorrarCategoria().then(fetchProductos).finally(setBorrarModalVisible(false))}}
+            />
+            <Button
+            style={styles.button}
+            title="Cancelar"
+            onPress={() => {setBorrarModalVisible(false)}}
+            />
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
@@ -18,4 +200,39 @@ const styles = StyleSheet.create({
     viewBody:{
         marginHorizontal: 30,
         },
+        modalBackGround:{
+          flex:1,
+          backgroundColor:'rgba(0,0,0,0,5)'
+          },
+          modalContainer:{
+            marginHorizontal: 30,
+          borderRadius: 16,
+          width:'80%',
+          backgroundColor:'white'
+          
+          },
+          text:{
+            flex:0.6
+          },
+          button: {
+            backgroundColor: '#859a9b',
+            borderRadius: 10,
+            padding: 5,
+            marginBottom: 10,
+            shadowColor: '#303838',
+            shadowOffset: { width: 0, height: 5 },
+            shadowRadius: 10,
+            shadowOpacity: 0.35,
+            maxHeight:30,
+            maxWidth:30,
+            flex:0.2,
+            alignSelf: 'flex-end',
+            right: 0,
+            flexDirection: 'row-reverse',
+          },
+          image: {
+            maxHeight:20,
+            maxWidth:20,
+            marginBottom: 20,
+          }
 })
